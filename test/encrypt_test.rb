23c0d7403f1505e1c45447d8af520d4020aafe48
Zach Stearns <zach.stearns92@gmail.com>
2020-09-18 22:56:37 -0600

    Add readable alphabet test

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -9,6 +9,12 @@ class EncryptTest < Minitest::Test
     assert_instance_of Encrypt, encrypt
   end
 
+def test_readable_alphabet
+  encrypt = Encrypt.new
+  expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+  assert_equal expected, encrypt.alphabet
+end
 
   def test_random_number_generator
     encrypt = Encrypt.new
