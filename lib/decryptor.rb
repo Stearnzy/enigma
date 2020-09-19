380ee29672223e652415c29a36ad9132c1c55de0
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 16:38:53 -0600

    Comment out rand num generator

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -5,9 +5,9 @@ require './lib/dateable'
 class Decryptor < Cryptograph
   include Dateable
 
-  def random_number_generator
-    rand.to_s[2..6]
-  end
+  # def random_number_generator
+  #   rand.to_s[2..6]
+  # end
 
   def split_string(string)
     string.downcase.split("").each_slice(4).to_a
