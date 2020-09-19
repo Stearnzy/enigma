0532e6ece98a0842650af80300166d31e9b87298
Zach Stearns <zach.stearns92@gmail.com>
2020-09-18 22:38:27 -0600

    Change letter_key to instance variable

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,7 +5,7 @@ class Encrypt
 
   include Dateable
   def initialize
-
+    @letter_keys = [:A, :B, :C, :D]
   end
 
   def random_number_generator
@@ -16,18 +16,13 @@ class Encrypt
     rand_num = random_number_generator
     placements = [rand_num[0..1], rand_num[1..2], rand_num[2..3], rand_num[3..4]]
     placements.map! {|placement| placement.to_i}
-
-    # letter_keys for inheritance..??
-    letter_keys = [:A, :B, :C, :D]
-    encryption_keys = Hash[letter_keys.zip(placements)]
+    encryption_keys = Hash[@letter_keys.zip(placements)]
   end
 
   def offset_generator
     last_four_date_squared = square_date[-4..-1]
     split_last_four = (last_four_date_squared.split(""))
     split_last_four.map! {|placement| placement.to_i}
-    # letter_keys are REPEATING
-    letter_keys = [:A, :B, :C, :D]
-    offset_values = Hash[letter_keys.zip(split_last_four)]
+    offset_values = Hash[@letter_keys.zip(split_last_four)]
   end
 end
