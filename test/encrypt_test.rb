f85ec2de274daf93409a5795b83be6b8b2723fe5
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 16:37:30 -0600

    Changed tests to reflect interation pattern

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -18,8 +18,8 @@ class EncryptTest < Minitest::Test
 
   def test_random_number_generator
     encrypt = Encrypt.new
-    encrypt.stubs(:random_number_generator).returns("02385")
-    assert_equal "02385", encrypt.random_number_generator
+    encrypt.stubs(:random_number_generator).returns("02715")
+    assert_equal "02715", encrypt.random_number_generator
   end
 
   def test_random_number_always_returns_5_digits
@@ -41,77 +41,77 @@ class EncryptTest < Minitest::Test
 
   def test_key_generator
     encrypt = Encrypt.new
-    encrypt.stubs(:random_number_generator).returns("02385")
-    expected = {A: 2, B: 23, C: 38, D: 85}
+    encrypt.stubs(:random_number_generator).returns("02715")
+    expected = {A: 2, B: 27, C: 71, D: 15}
     assert_equal expected, encrypt.key_generator
   end
 
   def test_square_date
     encrypt = Encrypt.new
-    encrypt.stubs(:date_conversion).returns("091820")
-    assert_equal "8430912400", encrypt.square_date
+    encrypt.stubs(:date_conversion).returns("040895")
+    assert_equal "1672401025", encrypt.square_date
   end
 
   def test_offset_generator
     encrypt = Encrypt.new
-    encrypt.stubs(:date_conversion).returns("091820")
-    expected = {A: 2, B: 4, C: 0, D: 0}
+    encrypt.stubs(:date_conversion).returns("040895")
+    expected = {A: 1, B: 0, C: 2, D: 5}
     assert_equal expected, encrypt.offset_generator
   end
 
   def test_master_shift_count
     encrypt = Encrypt.new
-    encrypt.stubs(:random_number_generator).returns("02385")
-    encrypt.stubs(:date_conversion).returns("091820")
-    expected = {A: 4, B: 27, C: 38, D:85}
+    encrypt.stubs(:random_number_generator).returns("02715")
+    encrypt.stubs(:date_conversion).returns("040895")
+    expected = {A: 3, B: 27, C: 73, D:20}
     assert_equal expected, encrypt.master_shift_count
   end
 
   def test_split_string
     encrypt = Encrypt.new
-    string = "Get Moving BROTHER"
-    expected = [["g", "e", "t", " "], ["m", "o", "v", "i"], ["n", "g", " ", "b"],
-                ["r", "o", "t", "h"], ["e", "r"]]
+    string = "HeLlO wOrLd"
+    expected = [["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]]
     assert_equal expected, encrypt.split_string(string)
   end
 
   def test_match_letter_to_shift
     encrypt = Encrypt.new
-    string = "Move Along!!"
-    encrypt.stubs(:random_number_generator).returns("02385")
-    encrypt.stubs(:date_conversion).returns("091820")
+    string = "Hello World!!"
+    encrypt.stubs(:random_number_generator).returns("02715")
+    encrypt.stubs(:date_conversion).returns("040895")
 
-    expected_1 = {A: 4, B: 27, C: 38, D:85}
+    expected_1 = {A: 3, B: 27, C: 73, D:20}
     assert_equal expected_1, encrypt.master_shift_count
 
-    expected_2 = [["m", [:A, 4]], ["o", [:B, 27]], ["v", [:C, 38]], ["e", [:D, 85]],
-                [" ", [:A, 4]], ["a", [:B, 27]], ["l", [:C, 38]], ["o", [:D, 85]],
-                ["n", [:A, 4]], ["g", [:B, 27]], ["!", [:C, 38]], ["!", [:D, 85]]]
+    expected_2 = [["h", [:A, 3]], ["e", [:B, 27]], ["l", [:C, 73]], ["l", [:D, 20]],
+                ["o", [:A, 3]], [" ", [:B, 27]], ["w", [:C, 73]], ["o", [:D, 20]],
+                ["r", [:A, 3]], ["l", [:B, 27]], ["d", [:C, 73]], ["!", [:D, 20]],
+                ["!", [:A, 3]]]
     assert_equal expected_2, encrypt.match_letter_to_shifts(string)
   end
 
   def test_total_shifts_per_character
     encrypt = Encrypt.new
-    string = "Move Along!!"
+    string = "Hello World!!"
 
-    encrypt.stubs(:random_number_generator).returns("02385")
-    encrypt.stubs(:date_conversion).returns("091820")
+    encrypt.stubs(:random_number_generator).returns("02715")
+    encrypt.stubs(:date_conversion).returns("040895")
 
-    expected_1 = {A: 4, B: 27, C: 38, D:85}
+    expected_1 = {A: 3, B: 27, C: 73, D:20}
     assert_equal expected_1, encrypt.master_shift_count
 
-    expected_2 = [16, 41, 59, 89, 30, 27, 49, 99, 17, 33, "!", "!"]
+    expected_2 = [10, 31, 84, 31, 17, 53, 95, 34, 20, 38, 76, "!", "!"]
     assert_equal expected_2, encrypt.shifts_per_character(string)
   end
 
   def test_string_encryption
     encrypt = Encrypt.new
-    encrypt.stubs(:random_number_generator).returns("02385")
-    encrypt.stubs(:date_conversion).returns("091820")
-    expected_1 = {A: 4, B: 27, C: 38, D:85}
+    encrypt.stubs(:random_number_generator).returns("02715")
+    encrypt.stubs(:date_conversion).returns("040895")
+    expected_1 = {A: 3, B: 27, C: 73, D:20}
     assert_equal expected_1, encrypt.master_shift_count
 
-    assert_equal "lewps gsvlo", encrypt.encrypt("hello world")
-    assert_equal "lenomnkgsowdhuoi!!!", encrypt.encrypt("Heckin Cool DUDE!!!")
+    assert_equal "keder ohulw!!", encrypt.encrypt("Hello World!!")
+    assert_equal "kevdlnswrodtguwy!!!", encrypt.encrypt("Heckin Cool DUDE!!!")
   end
 end
