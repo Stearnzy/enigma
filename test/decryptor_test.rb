40692f521268df9382895e4174df4b1aee8067e1
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 16:02:06 -0600

    Add random number tests

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -15,4 +15,28 @@ class DecryptorTest < Minitest::Test
       "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, decryptor.alphabet
   end
+
+  def test_random_number_generator
+    decryptor = Decryptor.new
+    decryptor.stubs(:random_number_generator).returns("02385")
+    assert_equal "02385", decryptor.random_number_generator
+  end
+
+  def test_random_number_always_returns_5_digits
+    decryptor = Decryptor.new
+    pass = 0
+    fail = 0
+    2000.times do
+      s = decryptor.random_number_generator
+      if s.length == 5
+        pass += 1
+      else
+        fail += 1
+      end
+    end
+
+    assert_equal 0, fail
+    assert_equal 2000, pass
+  end
+
 end
