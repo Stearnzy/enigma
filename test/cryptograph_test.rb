3f8cc85d3947aadf15959e3c311eb666576396f5
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 14:56:35 -0600

    Add readable alphabet test

--- test/cryptograph_test.rb
+++ test/cryptograph_test.rb
@@ -7,4 +7,11 @@ class CryptographTest < Minitest::Test
     cryptograph = Cryptograph.new
     assert_instance_of Cryptograph, cryptograph
   end
+
+  def test_readable_alphabet
+    cryptograph = Cryptograph.new
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, cryptograph.alphabet
+  end
 end
