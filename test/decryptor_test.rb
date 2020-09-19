8b412132034833a06d4516e2247f2d10a3738a45
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 16:39:58 -0600

    Comment out rand num tests and add split string test

--- test/decryptor_test.rb
+++ test/decryptor_test.rb
@@ -16,32 +16,34 @@ class DecryptorTest < Minitest::Test
     assert_equal expected, decryptor.alphabet
   end
 
-  def test_random_number_generator
-    decryptor = Decryptor.new
-    decryptor.stubs(:random_number_generator).returns("02715")
-    assert_equal "02715", decryptor.random_number_generator
-  end
-
-  def test_random_number_always_returns_5_digits
-    decryptor = Decryptor.new
-    pass = 0
-    fail = 0
-    2000.times do
-      s = decryptor.random_number_generator
-      if s.length == 5
-        pass += 1
-      else
-        fail += 1
-      end
-    end
-
-    assert_equal 0, fail
-    assert_equal 2000, pass
-  end
+  # def test_random_number_generator
+  #   decryptor = Decryptor.new
+  #   decryptor.stubs(:random_number_generator).returns("02715")
+  #   assert_equal "02715", decryptor.random_number_generator
+  # end
+  #
+  # def test_random_number_always_returns_5_digits
+  #   decryptor = Decryptor.new
+  #   pass = 0
+  #   fail = 0
+  #   2000.times do
+  #     s = decryptor.random_number_generator
+  #     if s.length == 5
+  #       pass += 1
+  #     else
+  #       fail += 1
+  #     end
+  #   end
+  #
+  #   assert_equal 0, fail
+  #   assert_equal 2000, pass
+  # end
 
   def test_split_string
     decryptor = Decryptor.new
     expected = [["k", "e", "d", "e"], ["r", " ", "o", "h"], ["u", "l", "w"]]
     assert_equal expected, decryptor.split_string("keder ohulw")
   end
+
+  
 end
