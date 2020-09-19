fb01e76adc907f554defc4aa249583bd91ea9c2b
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 16:09:46 -0600

    Add split string method

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -5,5 +5,11 @@ require './lib/dateable'
 class Decryptor < Cryptograph
   include Dateable
 
+  def random_number_generator
+    rand.to_s[2..6]
+  end
 
+  def split_string(string)
+    string.downcase.split("").each_slice(4).to_a
+  end
 end
