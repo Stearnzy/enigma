9e4953215bc3346e03564e00f4f893cdc21f7030
Zach Stearns <zach.stearns92@gmail.com>
2020-09-18 20:56:31 -0600

    Add test for random number generator

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,10 +1,25 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "mocha/minitest"
 require "./lib/encrypt"
 
 class EncryptTest < Minitest::Test
   def test_it_exists
     encrypt = Encrypt.new
     assert_instance_of Encrypt, encrypt
+  end
+
+
+  def test_random_number_generator
+    encrypt = Encrypt.new
+    encrypt.stubs(:random_number_generator).returns("02385")
+    assert_equal "02385", encrypt.random_number_generator
+  end
+
+  def test_key_generator
+    skip
+    encrypt = Encrypt.new
+    encrypt.generate_keys
+
   end
 end
