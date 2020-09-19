79f2c746c465a6d6daa7235c39b1c30c61a834ee
Zach Stearns <zach.stearns92@gmail.com>
2020-09-18 21:26:00 -0600

    Add generate keys method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,4 +6,12 @@ class Encrypt
   def random_number_generator
     rand.to_s[2..6]
   end
+
+  def generate_keys
+    rand_num = random_number_generator
+    placements = [rand_num[0..1], rand_num[1..2], rand_num[2..3], rand_num[3..4]]
+    placements.map! {|placement| placement.to_i}
+    letter_keys = [:A, :B, :C, :D]
+    encryption_keys = Hash[letter_keys.zip(placements)]
+  end
 end
