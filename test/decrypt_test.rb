324251f0721022ae3a453d238e21b2301ec90583
Zach Stearns <zach.stearns92@gmail.com>
2020-09-19 14:37:59 -0600

    Add existence test

--- test/decrypt_test.rb
+++ test/decrypt_test.rb
@@ -1,7 +1,11 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "mocha/minitest"
 require "./lib/decrypt"
 
 class DecryptTest < Minitest::Test
-
+  def test_it_exists
+    decrypt = Decrypt.new
+    assert_instance_of Decrypt, decrypt
+  end
 end
