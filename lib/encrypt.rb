d16603edead9533a468f7ecffaed68217c94fa70
Zach Stearns <zach.stearns92@gmail.com>
2020-09-18 22:30:33 -0600

    Add offset generator method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,3 +1,4 @@
+require 'date'
 require './lib/dateable'
 
 class Encrypt
@@ -15,9 +16,18 @@ class Encrypt
     rand_num = random_number_generator
     placements = [rand_num[0..1], rand_num[1..2], rand_num[2..3], rand_num[3..4]]
     placements.map! {|placement| placement.to_i}
+
+    # letter_keys for inheritance..??
     letter_keys = [:A, :B, :C, :D]
     encryption_keys = Hash[letter_keys.zip(placements)]
   end
 
-
+  def offset_generator
+    last_four_date_squared = square_date[-4..-1]
+    split_last_four = (last_four_date_squared.split(""))
+    split_last_four.map! {|placement| placement.to_i}
+    # letter_keys are REPEATING
+    letter_keys = [:A, :B, :C, :D]
+    offset_values = Hash[letter_keys.zip(split_last_four)]
+  end
 end
