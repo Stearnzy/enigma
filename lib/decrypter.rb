89c861fec9d793456df90aa5de5619bced49368f
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 16:50:29 -0600

    Rearrange and alter methods to mirror encrypter's

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -5,45 +5,41 @@ require './lib/dateable'
 class Decrypter < Cryptograph
   include Dateable
 
-  # def random_number_generator
-  #   rand.to_s[2..6]
-  # end
+# BOTH
+  def key_generator(key)
+    placements = [key[0..1], key[1..2], key[2..3], key[3..4]]
+    placements.map! {|placement| placement.to_i}
+    @key_shift = Hash[@letter_keys.zip(placements)]
+  end
 
-# Used in both encrypt, decrypt
+# BOTH
+  def offset_generator(date)
+    last_four_date_squared = square_date(date)[-4..-1]
+    split_last_four = (last_four_date_squared.split(""))
+    split_last_four.map! {|placement| placement.to_i}
+    @offset_shift = Hash[@letter_keys.zip(split_last_four)]
+  end
+
+# BOTH
+  def generate_master_offset
+    @master_key_shift = @key_shift.merge(@offset_shift) do |letter, key, off|
+      key + off
+    end
+  end
+
+# BOTH
   def split_string(string)
     string.downcase.split("").each_slice(4).to_a
   end
 
-# Used in both encrypt decrypt
-  def master_shift_count(keyset, offset)
-    @master_key_shift = keyset.merge(offset) do |letter, k, o|
-      k + o
-    end
-  end
-
-# Used in both encrypt and decrypt
-  def key_set_generator(key = random_number_generator)
-    placements = [key[0..1], key[1..2], key[2..3], key[3..4]]
-    placements.map! {|placement| placement.to_i}
-    encryption_keys = Hash[@letter_keys.zip(placements)]
-  end
-
+# BOTH
   def match_letter_to_shifts(string)
     split_string(string).map do |set_of_four_chars|
       set_of_four_chars.zip(@master_key_shift)
     end.flatten(1)
   end
 
-# Necessary method, used in both encrypt, decrypt
-# THIS HAS BEEN ALTERED
-  def offset_generator(date)
-    last_four_date_squared = square_date(date)[-4..-1]
-    split_last_four = (last_four_date_squared.split(""))
-    split_last_four.map! {|placement| placement.to_i}
-    offset_values = Hash[@letter_keys.zip(split_last_four)]
-  end
-
-# This method is what needs to change from + to -
+# DIFFERENT
   def index_shifts_per_character(string)
     match_letter_to_shifts(string).map do |letter_shift|
       if @alphabet.include?(letter_shift[0])
