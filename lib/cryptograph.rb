2ce54fe0aed75a53d0fd0e3041f73d49ca08bf94
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 19:28:34 -0600

    Add key generator method

--- lib/cryptograph.rb
+++ lib/cryptograph.rb
@@ -12,4 +12,11 @@ class Cryptograph
   def random_number_generator
     rand.to_s[2..6]
   end
+
+  def key_generator(key = random_number_generator)
+    placements = [key[0..1], key[1..2], key[2..3], key[3..4]]
+    placements.map! {|placement| placement.to_i}
+    @key_shift = Hash[@letter_keys.zip(placements)]
+  end
+
 end
