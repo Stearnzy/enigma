b142896f589b3473094b464638329ee44bcc1798
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 22:44:03 -0600

    Move encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,6 +8,23 @@ class Enigma < Cryptograph
   include Mappable
 
   def initialize
-    
+  end
+
+  def encrypt(string, key = random_number_generator, date = date_conversion)
+    key_generator(key)
+    offset_generator(date)
+    generate_master_offset
+    match_letter_to_shifts(string)
+    guide = encrypt_index_shifts_per_character(string)
+    index_mapping(guide)
+  end
+
+  def decrypt(string, key, date)
+    key_generator(key)
+    offset_generator(date)
+    generate_master_offset
+    match_letter_to_shifts(string)
+    guide = decrypt_index_shifts_per_character(string)
+    index_mapping(guide)
   end
 end
