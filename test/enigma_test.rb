dcb6e92dbf3c23270075256cd4f72028c28911d4
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 22:55:48 -0600

    Add key generator test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -45,4 +45,15 @@ class EnigmaTest < Minitest::Test
     assert_equal 0, fail
     assert_equal 2000, pass
   end
+
+  def test_key_generator
+    enigma = Enigma.new
+    enigma.stubs(:random_number_generator).returns("02715")
+
+    expected_1 = {A: 2, B: 27, C: 71, D: 15}
+    assert_equal expected_1, enigma.key_generator
+
+    expected_2 = {A: 6, B: 65, C: 59, D: 98}
+    assert_equal expected_2, enigma.key_generator("06598")
+  end
 end
