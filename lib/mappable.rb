6ca24d7fa78e02629b0bbad1de13a59fd368cc51
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 20:39:47 -0600

    Moved but commented ecryptor and decryptor methods, temporarily

--- lib/mappable.rb
+++ lib/mappable.rb
@@ -11,4 +11,42 @@ module Mappable
       end
     end.join
   end
+  #
+  # def encrypt_index_shifts_per_character(string)
+  #   match_letter_to_shifts(string).map do |letter_shift|
+  #     if @alphabet.include?(letter_shift[0])
+  #       @alphabet.index(letter_shift[0]) + letter_shift[1][1]
+  #     else
+  #       letter_shift[0]
+  #     end
+  #   end
+  # end
+  #
+  # def decrypt_index_shifts_per_character(string)
+  #   match_letter_to_shifts(string).map do |letter_shift|
+  #     if @alphabet.include?(letter_shift[0])
+  #       @alphabet.index(letter_shift[0]) - letter_shift[1][1]
+  #     else
+  #       letter_shift[0]
+  #     end
+  #   end
+  # end
+  #
+  # def encrypt(string, key = random_number_generator, date = date_conversion)
+  #   key_generator(key)
+  #   offset_generator(date)
+  #   generate_master_offset
+  #   match_letter_to_shifts(string)
+  #   guide = encrypt_index_shifts_per_character(string)
+  #   index_mapping(guide)
+  # end
+  #
+  # def decrypt(string, key, date)
+  #   key_generator(key)
+  #   offset_generator(date)
+  #   generate_master_offset
+  #   match_letter_to_shifts(string)
+  #   guide = decrypt_index_shifts_per_character(string)
+  #   index_mapping(guide)
+  # end
 end
