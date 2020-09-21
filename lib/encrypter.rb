c5fe07b104479eee1009351a49503f8c0ffcd510
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 20:40:23 -0600

    Specified method name

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -5,11 +5,12 @@ require './lib/mappable'
 
 # May be able to remove require date..?
 # Still need to give it output method, hash of encryption, key, date
+
 class Encrypter < Cryptograph
   include Dateable
   include Mappable
 
-  def index_shifts_per_character(string)
+  def encrypt_index_shifts_per_character(string)
     match_letter_to_shifts(string).map do |letter_shift|
       if @alphabet.include?(letter_shift[0])
         @alphabet.index(letter_shift[0]) + letter_shift[1][1]
@@ -24,7 +25,7 @@ class Encrypter < Cryptograph
     offset_generator(date)
     generate_master_offset
     match_letter_to_shifts(string)
-    guide = index_shifts_per_character(string)
+    guide = encrypt_index_shifts_per_character(string)
     index_mapping(guide)
   end
 end
