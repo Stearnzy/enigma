44876498244c5ab0c5f94e10bb8a169b2d5c1547
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 19:47:37 -0600

    Comment out index mapping method and include mappable

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -1,9 +1,11 @@
 require 'date'
 require './lib/cryptograph'
 require './lib/dateable'
+require './lib/mappable'
 
 class Decrypter < Cryptograph
   include Dateable
+  include Mappable
 
 # Warning : Might not be permanent here
   def random_number_generator
@@ -56,17 +58,17 @@ class Decrypter < Cryptograph
   end
 
 # BOTH
-  def index_mapping(input)
-    input.map do |index|
-      if index.is_a?(String)
-        index
-      elsif index > 27 || index < -27
-        @alphabet[index % 27]
-      else
-        @alphabet[index]
-      end
-    end.join
-  end
+  # def index_mapping(input)
+  #   input.map do |index|
+  #     if index.is_a?(String)
+  #       index
+  #     elsif index > 27 || index < -27
+  #       @alphabet[index % 27]
+  #     else
+  #       @alphabet[index]
+  #     end
+  #   end.join
+  # end
 
 # ONLY difference is METHOD NAME
   def decrypt(string, key, date)
