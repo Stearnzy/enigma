a0ee6ad5e074457ef325133de6b7c90bff08e881
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 19:56:52 -0600

    Delete methods moved to Cryptograph superclass

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -7,46 +7,6 @@ class Decrypter < Cryptograph
   include Dateable
   include Mappable
 
-# Warning : Might not be permanent here
-  def random_number_generator
-    rand.to_s[2..6]
-  end
-
-# BOTH
-  def key_generator(key = random_number_generator)
-    placements = [key[0..1], key[1..2], key[2..3], key[3..4]]
-    placements.map! {|placement| placement.to_i}
-    @key_shift = Hash[@letter_keys.zip(placements)]
-  end
-
-# BOTH
-  def offset_generator(date)
-    last_four_date_squared = square_date(date)[-4..-1]
-    split_last_four = (last_four_date_squared.split(""))
-    split_last_four.map! {|placement| placement.to_i}
-    @offset_shift = Hash[@letter_keys.zip(split_last_four)]
-  end
-
-# BOTH
-  def generate_master_offset
-    @master_shift = @key_shift.merge(@offset_shift) do |letter, key, off|
-      key + off
-    end
-  end
-
-# BOTH
-  def split_string(string)
-    string.downcase.split("").each_slice(4).to_a
-  end
-
-# BOTH
-  def match_letter_to_shifts(string)
-    split_string(string).map do |set_of_four_chars|
-      set_of_four_chars.zip(@master_shift)
-    end.flatten(1)
-  end
-
-# DIFFERENT
   def index_shifts_per_character(string)
     match_letter_to_shifts(string).map do |letter_shift|
       if @alphabet.include?(letter_shift[0])
@@ -57,20 +17,6 @@ class Decrypter < Cryptograph
     end
   end
 
-# BOTH
-  # def index_mapping(input)
-  #   input.map do |index|
-  #     if index.is_a?(String)
-  #       index
-  #     elsif index > 27 || index < -27
-  #       @alphabet[index % 27]
-  #     else
-  #       @alphabet[index]
-  #     end
-  #   end.join
-  # end
-
-# ONLY difference is METHOD NAME
   def decrypt(string, key, date)
     key_generator(key)
     offset_generator(date)
