756da4c269492dca353b9c26ee5ca11d6fbf6522
Zach Stearns <zach.stearns92@gmail.com>
2020-09-20 18:53:07 -0600

    Add index_decryption_mapping method

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -51,6 +51,18 @@ class Decrypter < Cryptograph
     compiled
   end
 
+  def index_decryption_mapping(input)
+    input.map do |index|
+      if index.is_a?(String)
+        index
+      elsif index > 27 || index < -27
+        @alphabet[index % 27]
+      else
+        @alphabet[index]
+      end
+    end.join
+  end
+
 
 # Args setup correctly, I think!
 # Key MUST be called for the DECRYPT method?
